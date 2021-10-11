5fde969b93537bb50cab74de8ef6d56f596ac685
soumodiptab <sbose2019@gmail.com>
2021-10-11 14:24:02 +0530

    admin left

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  aboutdata ='<p>helloworld</p><br><p>world</p>';
   constructor() { }
-
   ngOnInit(): void {
   }
 
